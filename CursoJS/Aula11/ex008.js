eff524df89a05def142b6223f2206ee5617a8b74
lipefelizardo <lipefelizardo123@gmail.com>
2024-07-12 23:30:08 -0300

    commit full course

---
+++ CursoJS/Aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 78.2
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if(vel > 60) { // Condição simples!
+    console.log('Você ultrapassou a velociade permitida. Multado!!')
+
+}
+console.log('dirige sempre usando cinto de segurança')
